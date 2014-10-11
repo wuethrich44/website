053217304e5e85920b439535a3577d9dd7ddad08
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2014-10-12 01:38:02 +0200

    Fix bjyauthorize

--- config/autoload/bjyauthorize.global.php
+++ config/autoload/bjyauthorize.global.php
@@ -2,41 +2,9 @@
 
 return array(
     'bjyauthorize' => array(
-        // set the 'guest' role as default (must be defined in a role provider)
         'default_role' => 'guest',
-        /* this module uses a meta-role that inherits from any roles that should
-         * be applied to the active user. the identity provider tells us which
-         * roles the "identity role" should inherit from.
-         *
-         * for ZfcUser, this will be your default identity provider
-         */
         'identity_provider' => 'BjyAuthorize\Provider\Identity\ZfcUserZendDb',
-        /* If you only have a default role and an authenticated role, you can
-         * use the 'AuthenticationIdentityProvider' to allow/restrict access
-         * with the guards based on the state 'logged in' and 'not logged in'.
-         *
-         * 'default_role'       => 'guest',         // not authenticated
-         * 'authenticated_role' => 'user',          // authenticated
-         * 'identity_provider'  => 'BjyAuthorize\Provider\Identity\AuthenticationIdentityProvider',
-         */
-
-        /* role providers simply provide a list of roles that should be inserted
-         * into the Zend\Acl instance. the module comes with two providers, one
-         * to specify roles in a config file and one to load roles using a
-         * Zend\Db adapter.
-         */
         'role_providers' => array(
-            /* here, 'guest' and 'user are defined as top-level roles, with
-             * 'admin' inheriting from user
-             */
-            'BjyAuthorize\Provider\Role\Config' => array(
-                'guest' => array(),
-                'user' => array('children' => array(
-                        'admin' => array(),
-                    )),
-            ),
-            // this will load roles from the user_role table in a database
-            // format: user_role(role_id(varchar), parent(varchar))
             'BjyAuthorize\Provider\Role\ZendDb' => array(
                 'table' => 'user_role',
                 'identifier_field_name' => 'id',
@@ -83,4 +51,3 @@ return array(
         ),
     ),
 );
-
