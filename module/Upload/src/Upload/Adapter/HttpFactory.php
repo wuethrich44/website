2f414e91d2eb0cb43fe25bde548512c0807722a1
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2014-10-13 18:41:54 +0200

    Refactor HttpFactory

---
+++ module/Upload/src/Upload/Adapter/HttpFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Upload\Adapter;
+
+use Zend\Validator\File\Size;
+use Zend\Validator\File\Extension;
+use Zend\Filter\File\Rename;
+use Zend\File\Transfer\Adapter\Http;
+
+class Http extends Http
+{
+
+    public function __construct($moduleOptions, $options = array())
+    {
+        parent::__construct($options);
+
+        $size      = new Size(array('max' => $moduleOptions->getMaxFileSizeInByte()));
+        $extension = new Extension($moduleOptions->getAllowedFileExtensions());
+
+        if ($moduleOptions->getRandomizeFileName()) {
+            $rename = new Rename(array(
+                'target' => $moduleOptions->getUploadFolderPath().'/file',
+                'randomize' => true,
+            ));
+        } else {
+            $rename = null;
+        }
+
+        $this->setValidators(array($size, $extension));
+        $this->setFilters(array($rename));
+    }
+
+}
--- module/Upload/src/Upload/Factory/HttpFactory.php
+++ module/Upload/src/Upload/Factory/HttpFactory.php
@@ -4,10 +4,7 @@ namespace Upload\Factory;
 
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\ServiceManager\FactoryInterface;
-use Zend\Validator\File\Size;
-use Zend\Validator\File\Extension;
-use Zend\Filter\File\Rename;
-use Zend\File\Transfer\Adapter\Http;
+use Upload\Adapter\Http;
 
 class HttpFactory implements FactoryInterface
 {
@@ -16,22 +13,6 @@ class HttpFactory implements FactoryInterface
     {
         $options = $serviceLocator->get('Upload\Options\ModuleOptions');
 
-        $size      = new Size(array('max' => $options->getMaxFileSizeInByte()));
-        $extension = new Extension($options->getAllowedFileExtensions());
-
-        if ($options->getRandomizeFileName()) {
-            $rename = new Rename(array(
-                'target' => $options->getUploadFolderPath().'/file',
-                'randomize' => true,
-            ));
-        } else {
-            $rename = null;
-        }
-
-        $adapter = new Http();
-        $adapter->setValidators(array($size, $extension));
-        $adapter->setFilters(array($rename));
-
-        return $adapter;
+        return new Http($options);
     }
 }
